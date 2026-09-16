cbec8eca81c6b95fd7771457f0a9ea0c7c016398
sarthakavahd <avhadsarthak908@gmail.com>
2026-09-17 02:32:59 +0530

    Revert "Fix playback engine race conditions, unify under native audio, remove iframe, enforce strict sync autoplay"
    This reverts commit 1e7e913499e6968a6881ed93e3cafb9cf18995ef.

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/app/page.tsx', 'utf-8');
-code = code.replace(/  };\s*setLyrics\(\[\]\);/g, '    setLyrics([]);');
-fs.writeFileSync('src/app/page.tsx', code);
